7865796931d637a0fe9e2c2e48ac776d30f8e1eb
Cory Charlton <ccharlton@pobox.com>
2023-10-29 11:58:17 -0700

    Adding unit tests

---
+++ CCSWE.nanoFramework.Core.UnitTests/StringsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using nanoFramework.TestFramework;
+
+namespace CCSWE.nanoFramework.Core.UnitTests
+{
+    [TestClass]
+    public class StringsTest
+    {
+        [TestMethod]
+        public void Join_should_throw_if_values_is_null()
+        {
+            Assert.ThrowsException(typeof(ArgumentNullException), () => Strings.Join(string.Empty, null));
+        }
+    }
+}
