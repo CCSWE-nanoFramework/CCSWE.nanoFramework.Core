a3c406df2f04083e54e4a9bfb320831b1094707a
Cory Charlton <ccharlton@pobox.com>
2023-11-17 19:12:02 -0800

    Fixing build

---
+++ CCSWE.nanoFramework.Core.UnitTests/IO/PathTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CCSWE.nanoFramework.IO;
+using nanoFramework.TestFramework;
+
+namespace CCSWE.nanoFramework.Core.UnitTests.IO
+{
+    [TestClass]
+    public class PathTests
+    {
+        [TestMethod]
+        public void GetExtension_returns_empty_string()
+        {
+            Assert.AreEqual(string.Empty, Path.GetExtension(string.Empty));
+            Assert.AreEqual(string.Empty, Path.GetExtension("file"));
+            Assert.AreEqual(string.Empty, Path.GetExtension("file."));
+        }
+
+        [TestMethod]
+        public void GetExtension_returns_extension()
+        {
+            var file = "file.ext";
+            var expect = ".ext";
+
+            Assert.AreEqual(expect, Path.GetExtension(file));
+            Assert.AreEqual(expect, Path.GetExtension($"I:{file}"));
+            Assert.AreEqual(expect, Path.GetExtension(@$"I:\{file}"));
+            Assert.AreEqual(expect, Path.GetExtension(@$"I:\directory\{file}"));
+            Assert.AreEqual(expect, Path.GetExtension(@$"\{file}"));
+            Assert.AreEqual(expect, Path.GetExtension(@$"\\server\share\{file}"));
+            Assert.AreEqual(expect, Path.GetExtension(@$"\\server\share\directory\{file}"));
+        }
+
+        [TestMethod]
+        public void GetExtension_returns_null()
+        {
+            Assert.IsNull(Path.GetExtension(null));
+        }
+    }
+}
