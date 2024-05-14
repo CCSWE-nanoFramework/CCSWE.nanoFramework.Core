6b77205c4e2a4a0075506500be6c562d5cc10859
Cory Charlton <ccharlton@pobox.com>
2024-05-14 14:23:50 -0700

    Updating nanoFramework.System.Runtime

--- CCSWE.nanoFramework.Core.UnitTests/ThrowHelperTests.cs
+++ CCSWE.nanoFramework.Core.UnitTests/ThrowHelperTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using nanoFramework.TestFramework;
 
 namespace CCSWE.nanoFramework.Core.UnitTests
@@ -23,17 +22,47 @@ namespace CCSWE.nanoFramework.Core.UnitTests
 
             try
             {
+                // ReSharper disable once ExpressionIsAlwaysNull
                 ThrowHelper.ThrowIfNull(unitTestParameter);
             }
             catch (ArgumentNullException ex)
             {
                 Assert.AreEqual(expect, ex.ParamName);
                 testExecuted = true;
-
-                Debug.WriteLine(ex.Message);
             }
 
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
             Assert.IsTrue(testExecuted);
         }
+
+        [TestMethod]
+        public void ThrowIfNullOrEmpty_throws_ArgumentNullException()
+        {
+            Assert.ThrowsException(typeof(ArgumentNullException), () => ThrowHelper.ThrowIfNullOrEmpty(null));
+
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrEmpty_throws_ArgumentNullException_with_correct_ParamName()
+        {
+            string unitTestParameter = null;
+            const string expect = nameof(unitTestParameter);
+            var testExecuted = false;
+
+            try
+            {
+                // ReSharper disable once ExpressionIsAlwaysNull
+                ThrowHelper.ThrowIfNullOrEmpty(unitTestParameter);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual(expect, ex.ParamName);
+                testExecuted = true;
+            }
+
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+            Assert.IsTrue(testExecuted);
+        }
+
     }
 }
